d7fc40434f15dee10b0a80fcc13012587bbee7a9
dyshleva <116552489+dyshleva@users.noreply.github.com>
2023-11-27 22:28:09 +0200

    Delete demo directory

--- demo/src/main/java/com/example/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Main {
-    public static void main(String[] args) {
-        Document sd = new SmartDocument("/Users/dyshleva/Desktop/lab10/demo/src/main/java/com/example/my.png");
-        System.out.println(sd.parse());
-    }
-}
